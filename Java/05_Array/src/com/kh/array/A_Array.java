727224773d91be0f28776bc65d15fe56b546e1df
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-16 17:45:22 +0900

    A_Array

--- Java/05_Array/src/com/kh/array/A_Array.java
+++ Java/05_Array/src/com/kh/array/A_Array.java
@@ -1,5 +1,6 @@
 package com.kh.array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A_Array {
@@ -68,11 +69,142 @@ public class A_Array {
 	}
 	
 	
+	/*
+	 * 3명의 키를 입력받아 배열에 저장하고 3명의 키의 평균값을 구하시오.
+	 * 키 입력 > 180
+	 * 키 입력 > 177.3
+	 * 키 입력 > 168.2
+	 * 175.2
+	 * 
+	 * */
+	
+	public void method3() {
+		
+		System.out.print("키 입력 > ");
+		double height1 = sc.nextDouble();
+		System.out.print("키 입력 > ");
+		double height2 = sc.nextDouble();
+		System.out.print("키 입력 > ");
+		double height3 = sc.nextDouble();
+		
+		double[] heights = new double[3];
+		heights[0] = height1;
+		heights[1] = height2;
+		heights[2] = height3;
+		double sum = 0;
+		for(int i=0; i<3; i++) {
+			sum += heights[i];
+		}
+		System.out.printf("%.1f",(sum/3));
+
+	// [강사님]
+		
+		double[] arr = new double[3];
+		double sum1 = 0;
+		for(int i=0; i<arr.length; i++) {
+			System.out.print("키 입력 > ");
+			arr[i] = sc.nextDouble();	
+			sum1 += arr[i];
+		}
+			//System.out.println(Arrays.toString(arr));
+			System.out.printf("%.1f", sum1 / arr.length);
+	}
+	
+	// 배열의 복사 
+	// 1. 얕은 복사 : 배열의 주소만 복사
+	public void method4() {
+		int[] number = {1, 2, 3, 4, 5};
+		int[] copy = number;
+		
+		System.out.println(number);
+		System.out.println(copy);
+		
+		copy[1] = 20;
+		
+		System.out.println(Arrays.toString(copy));
+		System.out.println(Arrays.toString(number));
+		
+	}
+	// 2. 깊은 복사 : 동일한 새로운 배열을 하나 생성해서 내부 값들도 함께 복사 
+	// 1) for 문을 이용한 깊은 복사
+	public void method5() {
+		int[] number = {1,2,3,4,5};
+		int[] copy = new int[number.length];  //number.length = 5
+		
+		for(int i = 0; i<number.length; i++) {
+			copy[i] = number[i];
+		}
+		
+		System.out.println(number);
+		System.out.println(copy);        // 둘이 주소값도 다르고 
+		
+		copy[1] = 20;
+		
+		System.out.println(Arrays.toString(copy));
+		System.out.println(Arrays.toString(number));    // 배열 안 숫자도 다름 
+	}
+	
+	// 2) System 클래스에서 제공하는 arraycopy() 메서드를 이용한 깊은 복사 
+	// 	  System.arraycopy(원본배열, 복사시작인덱스, 복사본배열, 복사시작인덱스, 복사할길이);
+	public void method6() {
+		int[] number = {1,2,3,4,5};
+		int[] copy = new int[number.length];  //number.length = 5
+		
+		System.arraycopy(number, 0, copy, 0, number.length);
+		
+		System.out.println(number);
+		System.out.println(copy);        // 둘이 주소값도 다르고 
+		
+		copy[1] = 20;
+		
+		System.out.println(Arrays.toString(copy));
+		System.out.println(Arrays.toString(number));    // 배열 안 숫자도 다름
+		
+	}
+	
+	// 3) Arrays 클래스에서 제공하는 copyOF() 메서드를 이용한 깊은 복사 
+	//    Arryas.copyOf(원본배열, 복사본배열길이);
+	public void method7() {
+		int[] number = {1,2,3,4,5};
+		int[] copy = Arrays.copyOf(number, number.length);
+		
+		System.out.println(number);
+		System.out.println(copy);        // 둘이 주소값도 다르고 
+		
+		copy[3] = 10;
+		
+		System.out.println(Arrays.toString(copy));
+		System.out.println(Arrays.toString(number));    // 배열 안 숫자도 다름
+	}
+	
+	// 4) 배열의 clone() 메서드를 이용한 깊은 복사 (얘만 알면 됨!)
+	public void method8() {
+		int[] number = {1,2,3,4,5};
+		int[] copy = number.clone();
+		
+		System.out.println(number);
+		System.out.println(copy);        // 둘이 주소값도 다르고 
+		
+		copy[3] = 20;
+		
+		System.out.println(Arrays.toString(copy));
+		System.out.println(Arrays.toString(number));    // 배열 안 숫자도 다름
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		A_Array a = new A_Array();
 		// a.method1();
-		a.method2();
+		// a.method2();
+//		a.method3();
+//		a.method4();
+//		a.method5();
+//		a.method5();
+//		a.method6();
+//		a.method7();
+		a.method8();
+		
 		
 	}
 
