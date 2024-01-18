cc91fe1ffc0411596f3ee321b805374be38d1c52
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-18 17:18:53 +0900

    Tv

--- Java/07_Inheritance/src/com/kh/inheritance/child/Tv.java
+++ Java/07_Inheritance/src/com/kh/inheritance/child/Tv.java
@@ -5,4 +5,26 @@ import com.kh.inheritance.parent.Product;
 public class Tv extends Product{
 		
 	private int inch;        // 인치
+
+	public Tv() {}
+
+	public Tv(String brand, String pCode, String name, int price, int inch) {
+		super(brand, pCode, name, price);
+		this.inch = inch;
+	}
+
+	public int getInch() {
+		return inch;
+	}
+
+	public void setInch(int inch) {
+		this.inch = inch;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()+", Tv [inch=" + inch + "]";
+	}
+	
+	
 }
