84a363906357eb8c8b0a416be46cf74c8640a619
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-18 17:18:48 +0900

    SmartPhone

--- Java/07_Inheritance/src/com/kh/inheritance/child/SmartPhone.java
+++ Java/07_Inheritance/src/com/kh/inheritance/child/SmartPhone.java
@@ -6,4 +6,27 @@ public class SmartPhone extends Product {
 
 	private String mobileAgency;   // 통신사
 
+	public SmartPhone() {}
+
+	public SmartPhone(String brand, String pCode, String name, int price, String mobileAgency) {
+		super(brand, pCode, name, price);
+		this.mobileAgency = mobileAgency;
+	}
+
+	public String getMobileAgency() {
+		return mobileAgency;
+	}
+
+	public void setMobileAgency(String mobileAgency) {
+		this.mobileAgency = mobileAgency;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()+", SmartPhone [mobileAgency=" + mobileAgency + "]";
+	}
+	
+	
+	
+	
 }
