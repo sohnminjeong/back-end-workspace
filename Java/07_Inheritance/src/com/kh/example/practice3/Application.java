e9d44147a8a6f90a2cd34557f2fb0f5c9f2dc903
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-18 17:18:15 +0900

    Application

---
+++ Java/07_Inheritance/src/com/kh/example/practice3/Application.java
@@ -0,0 +1,135 @@
+package com.kh.example.practice3;
+
+import java.util.Scanner;
+
+import com.kh.example.practice3.controller.EmployeeController;
+import com.kh.example.practice3.model.Employee;
+
+public class Application {
+	// 전역변수로 쓰기 위해서 main안이 아닌 밖에 지정! 
+	Scanner sc = new Scanner(System.in);
+	Employee viewEmployee = new Employee();
+	EmployeeController employeeController = new EmployeeController();
+
+	public static void main(String[] args) {
+
+		Application app = new Application();
+
+		boolean check = true;
+		while(check) {
+			int select = app.employeeMenu();
+			switch(select) {
+			case 1 : 
+				app.insertEmp();
+				break;
+			case 2 :
+				app.updateEmp();
+				break;
+			case 3 :
+				app.printEmp();
+				break;
+			case 9 :
+				System.out.println("프로그램을 종료합니다.");
+				check = false;
+				break;
+			default : 
+				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요~");
+			}
+		}	
+	} 
+		
+		
+	// 메인 메뉴를 출력
+	// 인스턴스 메서드 : 객체가 생성 되야 쓸 수 있음!
+	// 메인은 스태틱 메서드 : 바로 실행 가능! 
+	public int employeeMenu() {
+		System.out.println("1. 사원 정보 추가");
+		System.out.println("2. 사원 정보 수정");
+		System.out.println("3. 사원 정보 출력");
+		System.out.println("9. 프로그램 종료");
+		System.out.print("메뉴 번호를 누르세요 : ");
+//		int menu = Integer.parseInt(sc.nextLine());
+		return Integer.parseInt(sc.nextLine());
+	}
+	
+	
+	// 저장할 데이터를 사용자에게 받는 메소드 
+	public void insertEmp() {
+		System.out.print("사원 번호 : ");
+		int empNo = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("사원 이름 : ");
+		String name = sc.nextLine();
+		
+		System.out.print("사원 성별 : ");
+		char gender = sc.nextLine().charAt(0);
+		
+		System.out.print("전화 번호 : ");
+		String phone = sc.nextLine();
+		
+		System.out.print("추가 정보를 더 입력하시겠습니까?(y/n) : " );
+		String add = sc.nextLine();
+		
+		if(add.equals("y")) {
+			System.out.print("사원 부서 : ");
+			String dept = sc.nextLine();
+			
+			System.out.print("사원 연봉 : ");
+			int salary = Integer.parseInt(sc.nextLine());
+			
+			System.out.print("보너스 율 : ");
+			// 문자열을 더블 타입으로 바꾸는 것(int타입으로 바꾸는 것과 동일한 역할) 
+			double bonus = Double.parseDouble(sc.nextLine());			
+		
+			// 추가 정보까지! 
+			employeeController.add(empNo, name, gender, phone, dept, salary, bonus);
+			
+		} else {
+			// else인 경우엔 추가정보 추가가 없기 때문에 해당 메소드를 출력
+			employeeController.add(empNo, name, gender, phone);
+		}
+	}
+
+	
+	
+	// 수정할 데이터를 사용자에게 받는 메소드 
+	public void updateEmp() {
+		
+		System.out.println("사원의 어떤 정보를 수정하시겠습니까?");
+		System.out.println("1. 전화 번호");
+		System.out.println("2. 사원 연봉");
+		System.out.println("3. 보너스 율");
+		System.out.println("9. 돌아가기");
+		System.out.print("메뉴 번호를 누르세요 : ");
+		int select = Integer.parseInt(sc.nextLine());
+		
+		switch(select) {
+		case 1 : 
+			System.out.print("전화 번호 입력 : ");
+			employeeController.modify(sc.nextLine());
+			// String phone = sc.nextLine();
+			// employeeController.modify(phone);
+			break;
+		case 2 : 
+			System.out.print("사원 연봉 입력 : ");
+			employeeController.modify(Integer.parseInt(sc.nextLine()));
+			// int salary = Integer.parseInt(sc.nextLine());
+			// employeeController.modify(salary);
+			break;
+		case 3 :
+			System.out.print("보너스 율 입력 : ");
+			employeeController.modify(Double.parseDouble(sc.nextLine()));
+			// double bonus = Double.parseDouble(sc.nextLine());
+			// employeeController.modify(bonus);
+			break;
+		case 9 : 
+			break;
+		}
+	}
+	
+	
+	// 데이터를 출력하는 메소드
+	public void printEmp() {
+		System.out.println(employeeController.info());
+	}
+}
