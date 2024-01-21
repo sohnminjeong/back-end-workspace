3d17d58670f5980a32824364743f5c2785440e32
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-21 20:47:25 +0900

    MemberControllers(practice)

--- Java/08_Polymorphism/src/com/kh/array/practice2/controller/MemberControllers.java
+++ Java/08_Polymorphism/src/com/kh/array/practice2/controller/MemberControllers.java
@@ -1,5 +1,7 @@
 package com.kh.array.practice2.controller;
 
+import java.util.Arrays;
+
 import com.kh.array.practice2.model.Members;
 
 public class MemberControllers {
@@ -7,9 +9,9 @@ public class MemberControllers {
 	public int count = 0;
 	private Members[] mArr = new Members[3];
 	
-	public void insertMember(String id, String name, String password, String email, char gender, int age) {
+	public void insertMember(Members m) {
 		
-		
+		/*
 		mArr[count].setId(id);
 		mArr[count].setName(name);
 		mArr[count].setPassword(password);
@@ -17,16 +19,44 @@ public class MemberControllers {
 		mArr[count].setGender(gender);
 		mArr[count].setAge(age);
 			count+=1;
-			
+		*/
+		mArr[count++] = new Members(m.getId(), m.getName(), m.getPassword(), m.getEmail(), m.getGender(), m.getAge());
+//		System.out.println(Arrays.toString(mArr)); 확인용
 		
-//			System.out.println(mArr);
-	}
-	
-	public void updateMember() {
 		
 	}
+	/*
+	// 아이디가 중복인 경우
+		public boolean idCheck(String id) {
+			for(Members m : mArr) {
+				if(m != null && m.getId().equals(id)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	*/
+	// 아이디가 있는, 없는 경우 -> 배열의 index번호로 찾기
+	public int idCheck(String id) {
+		for(int i = 0; i < mArr.length; i++) {
+			if(mArr[i] != null && mArr[i].getId().equals(id)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 	
-	public void printAll() {
+	public void updateMember(String id, String name, String email, String password) {
 		
+		int index = idCheck(id);   // 아이디 중복 유무 확인 시 중복의 경우 true값이 i이기 때문
+		mArr[index].setName(name);
+		mArr[index].setEmail(email);
+		mArr[index].setPassword(password);
+		
+	}
+	
+	// 반환형이 Members[] 
+	public Members[] printAll() {
+		return mArr;
 	}
 }
