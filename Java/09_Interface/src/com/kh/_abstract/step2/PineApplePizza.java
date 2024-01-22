777b494273cf7d1610ef94ada1dca73214d3a52a
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-22 17:51:13 +0900

    abstract

---
+++ Java/09_Interface/src/com/kh/_abstract/step2/PineApplePizza.java
@@ -0,0 +1,12 @@
+package com.kh._abstract.step2;
+
+public class PineApplePizza extends Pizza{
+		
+	public PineApplePizza(int price, String brand) {
+		super(price, brand);
+	}
+
+	public void topping() {
+		System.out.println("PineApple Topping ...");
+	}; 
+}
---
+++ Java/09_Interface/src/com/kh/_abstract/step2/PotatoPizza.java
@@ -0,0 +1,12 @@
+package com.kh._abstract.step2;
+
+public class PotatoPizza extends Pizza{
+	
+	public PotatoPizza(int price, String brand) {
+		super(price, brand);
+	}
+
+	public void topping() {
+		System.out.println("Potato Topping ...");
+	}; 
+}
