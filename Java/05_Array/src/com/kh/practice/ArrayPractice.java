57855d057370f2b1779a910917556b2349a824d5
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-23 17:06:37 +0900

    ArrayPractice

--- Java/05_Array/src/com/kh/practice/ArrayPractice.java
+++ Java/05_Array/src/com/kh/practice/ArrayPractice.java
@@ -123,12 +123,10 @@ public class ArrayPractice {
 	 * */
 	public void method5() {
 		
-		
 		System.out.print("단어 입력 : ");
 		String input = sc.nextLine();
 		
 		char[] arr = input.toCharArray();
-		
 		
 		for(int i = arr.length-1; i>=0; i--) {
 			System.out.print(arr[i]);
