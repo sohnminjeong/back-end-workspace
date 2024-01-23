9929f7b708c601c010dc00da2e021287293f0198
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-23 17:06:20 +0900

    E_SubClass

---
+++ Java/10_Exception/src/com/kh/practice1/E_SubClass.java
@@ -0,0 +1,42 @@
+package com.kh.practice1;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class E_SubClass extends E_SuperClass {
+	
+//	1) @Override
+//	public void method() {
+//		// 오버라이딩 시 throws 작성하지 않아도 됨 (작성한다고 해서 문제 생기는 것x)
+//	}
+	
+//	2) @Override
+//	public void method() throws ClassNotFoundException {
+//		// 부모 클래스의 메서드와 다른 Exception을 throws 하는 것은 불가능
+//	}
+	
+
+	
+//	3) @Override
+//	public void method() throws IOException {
+//		// 부모 클래스의 메서드와 같은 Exception을 throws 하는 것은 가능
+//	}
+	
+//	@Override
+//	public void method() throws FileNotFoundException {
+//		// FileNotFoundException : IOException의 자식 
+//		// 부모 클래스의 메서드보다 더 하위 타입의 Exception을 throws 하는 것은 가능
+//	}
+	
+	
+//	@Override
+//	public void method() throws Exception { 
+//		// Excepton : IOException의 부모
+//		// 부모 클래스의 메서드보다 더 상위 타입의 Exception을 throws 하는 것은 불가능
+//	}	
+	
+	
+	
+	
+
+}
