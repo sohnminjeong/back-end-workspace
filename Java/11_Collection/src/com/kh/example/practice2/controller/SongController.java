2626ee230b1a01456613560674dd44a5c0e57d87
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-26 17:47:43 +0900

    SongController

---
+++ Java/11_Collection/src/com/kh/example/practice2/controller/SongController.java
@@ -0,0 +1,12 @@
+package com.kh.example.practice2.controller;
+
+
+public class SongController {
+	
+	public void backAdd(String title, String singer) {
+		
+//		this.title = title;
+//		this.singer = singer;
+		
+	}
+}
