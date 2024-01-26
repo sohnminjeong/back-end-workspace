178a78a4f75aefd7c7078cfce6987bc15e49455a
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-26 17:47:47 +0900

    Song

---
+++ Java/11_Collection/src/com/kh/example/practice2/model/Song.java
@@ -0,0 +1,38 @@
+package com.kh.example.practice2.model;
+
+public class Song implements Comparable<Song>{
+	private String title;
+	private String singer;
+	
+	public Song() {
+	}
+	public Song(String title, String singer) {
+		this.title = title;
+		this.singer = singer;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+	@Override
+	public String toString() {
+//		return "Song [title=" + title + ", singer=" + singer + "]";
+		return singer + " - " + title;
+	}
+	
+	@Override
+	public int compareTo(Song o) {
+		return this.title.compareTo(o.title);
+	}
+	
+	
+}
