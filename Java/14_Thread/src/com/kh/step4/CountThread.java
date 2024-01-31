7a8710f7f88905d90c0fd5a8ab6a4f4b61f4947f
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-01-31 17:24:19 +0900

    CountThread

---
+++ Java/14_Thread/src/com/kh/step4/CountThread.java
@@ -0,0 +1,15 @@
+package com.kh.step4;
+
+public class CountThread implements Runnable {
+
+	public void run() {
+		for(int i=10; i>0; i--) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+	}
+}
