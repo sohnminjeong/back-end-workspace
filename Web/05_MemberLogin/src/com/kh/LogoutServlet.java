7ace160a1a4a22d71fa499de34e731249090c283
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-02-13 18:22:57 +0900

    MemberLogin

---
+++ Web/05_MemberLogin/src/com/kh/LogoutServlet.java
@@ -0,0 +1,27 @@
+package com.kh;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kh.model.vo.Member;
+
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 로그아웃 : 세션 죽여서 로그아웃하고 index.jsp로 오면 됨 
+		HttpSession session = request.getSession();
+		Member member = (Member) session.getAttribute("login");
+		
+		session.invalidate();
+		response.sendRedirect("index.jsp");
+	}
+
+}
---
+++ Web/05_MemberLogin/src/com/kh/RegisterServlet.java
@@ -0,0 +1,47 @@
+package com.kh;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.model.dao.MemberDAO;
+import com.kh.model.vo.Member;
+
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 폼 값 받기
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		
+		// VO 작성
+		Member member = new Member(id, password, name);
+		
+		// DAO 리턴 받기
+		MemberDAO dao = new MemberDAO();
+		
+		try {
+			dao.register(member);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// 바인딩
+		request.setAttribute("register", member);
+		
+		// 네비게이션
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		
+	}
+
+}
